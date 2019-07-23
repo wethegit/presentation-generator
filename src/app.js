fe1a17819b3e3f24fbbdd3b909bd1aeb99266093
Marlon Marcello <marlon.marcello@gmail.com>
2019-07-22 17:02:37 -0700

    Cleaned the tree generator and updated template

--- src/app.js
+++ src/app.js
@@ -26,27 +26,38 @@ const appDir = jetpack.cwd(app.getAppPath());
 
 // Start of the app
 const appElement = document.querySelector("#app");
-const feedbackElement = appElement.querySelector('.app__feedback');
-const acceptedFolders = ['mobile', 'tablet', 'desktop'];
+const feedbackElement = document.querySelector('.feedback');
+const feedbackMessage = feedbackElement.querySelector('.feedback__message');
+const acceptedFolders = ['mobile', 'tablet', 'desktop', 'intro'];
+const appForm = appElement.querySelector('.form');
 const ignore = ['.DS_Store'];
 
 let defaultPath = store.get('dropzone_path');
 let feedbackDelay = null;
-let tree = null;
 let state = 'idle';
 
-const giveFeedback = function(message) {
+const giveFeedback = function(message, autoRemove = true) {
   clearTimeout(feedbackDelay);
-  feedbackElement.innerHTML = message;
+  feedbackElement.classList.add('is-visible');
+  feedbackMessage.innerHTML = message;
+
+  if (autoRemove) {
+    feedbackDelay = setTimeout(() => {
+      feedbackElement.classList.remove('is-visible');
+    }, 2000);
+  }
 }
 
 const setState = function(value) {
   appElement.classList.remove(`is-${state}`);
   state = value;
-  appElement.classList.add(`is-${state}`);
+  if (value)
+    appElement.classList.add(`is-${state}`);
 }
 
-const onClick = function() {
+const onSubmit = function(event) {
+  event.preventDefault();
+
   if (state === 'processing') return;
 
   const selectedFolder = dialog.showOpenDialog({
@@ -57,83 +68,89 @@ const onClick = function() {
 
   if (selectedFolder) {
     setState('processing');
-    giveFeedback('Processing...')
+    giveFeedback('Processing...', false);
 
     defaultPath = selectedFolder[0];
     store.set('defaultPath', { defaultPath: defaultPath });
 
-    tree = jetpack.inspectTree(defaultPath);
+    let tree = {};
+    tree.folders = sanitizeFolder(jetpack.inspectTree(defaultPath));
+
+    const formData = new FormData(appForm);
+    for (let [key, value] of formData.entries()) {
+      tree[key] = value;
+    }
+
     console.log(tree);
+    generateTemplate(tree);
   }
 }
 
 // sanity check of the folder structure
-const onTreeUpdate = function (tree, rootNode) {
+const sanitizeFolder = function (tree) {
   let passCheck = false;
   let cleanData = {};
 
-  console.log(rootNode)
+  for (let child of tree.children) {
+    if (acceptedFolders.indexOf(child.name.toLowerCase()) >= 0) {
+      if (child.children) {
+        cleanData[child.name] = cleanData[child.name] || [];
 
-  for (let [folder, files] of Object.entries(tree[rootNode.name])) {
-    if (acceptedFolders.indexOf(folder) >= 0) {
-      cleanData[folder] = cleanData[folder] || {};
-
-      for (let [filename, fileData] of Object.entries(files)) {
-        if (ignore.indexOf(filename) < 0) {
-          passCheck = true;
-          cleanData[folder][filename] = fileData;
+        for (let file of child.children) {
+          if (ignore.indexOf(file.name) < 0) {
+            passCheck = true;
+            cleanData[child.name].push(`./${child.name}/${file.name}`);
+          }
         }
       }
     }
   }
 
-  console.log(cleanData)
-
   if (!passCheck) {
     giveFeedback('Folder structure is wrong');
     return;
   }
+
+  return cleanData;
 }
 
-const generateTemplate = function (tree, rootNode) {
+const generateTemplate = function (tree) {
   let compiledFunction;
   try {
     compiledFunction = pug.compileFile(appDir.path("app/template.pug"));
   } catch (error) {
-    giveFeedback('Error while compiling template.');
-    console.log(error)
+    giveFeedback('Error while compiling template, try again.');
+    console.log(error);
     return;
   }
 
   const data = {
-    tree: {
-      ...cleanData
-    },
+    ...tree
   };
 
   let htmlString;
   try {
     htmlString = compiledFunction(data);
   } catch (error) {
-    giveFeedback('Error while generating HTML.');
+    giveFeedback('Error while generating HTML, please try again.');
     console.log(error);
     return;
   }
 
   dialog.showSaveDialog({
     title: 'Save file',
-    defaultPath: `${rootNode.fullPath}/index.html`
+    defaultPath: `${defaultPath}/index.html`
   }, (filename) => {
-    // console.log(filename)
-    jetpack.write(filename, htmlString);
-  })
-
-  console.log(htmlString)
+    if (filename) {
+      console.log(filename);
+      jetpack.write(filename, htmlString);
+      giveFeedback('All done ✌️');
+      setState('');
+    }
+  });
 }
-
-// Set initial feedback
-giveFeedback('Click and pick a folder 👆');
 
 // show app and add event listener
 appElement.style.display = "flex";
-appElement.addEventListener('click', onClick);
+
+appForm.addEventListener('submit', onSubmit);
